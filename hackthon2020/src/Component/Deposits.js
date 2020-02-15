bef978d06f6474e85df5d1e3dba4d895a1b261ca
Haohu Shen <haohu.shen@ucalgary.ca>
2020-02-15 16:07:10 -0700

    update Boxes

--- hackthon2020/src/Component/Deposits.js
+++ hackthon2020/src/Component/Deposits.js
@@ -4,6 +4,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import Title from "./Title";
 
+
 function preventDefault(event) {
   event.preventDefault();
 }
@@ -14,22 +15,244 @@ const useStyles = makeStyles({
   }
 });
 
-export default function Deposits() {
+function BusinessLibrary() {
   const classes = useStyles();
   return (
     <React.Fragment>
-      <Title>Recent Deposits</Title>
-      <Typography component="p" variant="h4">
-        $3,024.00
+      <Title>Health Sciences Library</Title>
+      <Typography component="p" variant="h7">
+        Current Status:
       </Typography>
+      <Typography component="p" variant="h5">
+        CLOSED
+      </Typography>
+      <Typography component="p" variant="h7">
+        Current Number of People:
+      </Typography>
+      <Typography component="p" variant="h6">
+        0
+      </Typography>
+      <Typography component="p" variant="h6" color="#F44336">
+        Flow Rate: Low {/*Should be light green here! */}
+      </Typography>
+      
       <Typography color="textSecondary" className={classes.depositContext}>
-        on 15 March, 2019
       </Typography>
       <div>
         <Link color="primary" href="#" onClick={preventDefault}>
-          View balance
+          Check details
         </Link>
       </div>
     </React.Fragment>
   );
 }
+
+function HealthSciencesLibrary() {
+  const classes = useStyles();
+  return (
+    <React.Fragment>
+      <Title>Health Sciences Library</Title>
+      <Typography component="p" variant="h7">
+        Current Status:
+      </Typography>
+      <Typography component="p" variant="h5">
+        OPEN
+      </Typography>
+      <Typography component="p" variant="h7">
+        Current Number of People:
+      </Typography>
+      <Typography component="p" variant="h6">
+        52
+      </Typography>
+      <Typography component="p" variant="h6" color="#F44336">
+        Flow Rate: Middle {/*Should be green here! */}
+      </Typography>
+      
+      <Typography color="textSecondary" className={classes.depositContext}>
+      </Typography>
+      <div>
+        <Link color="primary" href="#" onClick={preventDefault}>
+          Check details
+        </Link>
+      </div>
+    </React.Fragment>
+  );
+}
+
+function TaylorFamilyDigitalLibrary() {
+  const classes = useStyles();
+  return (
+    <React.Fragment>
+      <Title>Taylor Family Digital Library</Title>
+      <Typography component="p" variant="h7">
+        Current Status:
+      </Typography>
+      <Typography component="p" variant="h5">
+        OPEN
+      </Typography>
+      <Typography component="p" variant="h7">
+        Current Number of People:
+      </Typography>
+      <Typography component="p" variant="h6">
+        125
+      </Typography>
+      <Typography component="p" variant="h6" color="#F44336">
+        Flow Rate: High {/*Should be red here! */}
+      </Typography>
+      
+      <Typography color="textSecondary" className={classes.depositContext}>
+      </Typography>
+      <div>
+        <Link color="primary" href="#" onClick={preventDefault}>
+          Check details
+        </Link>
+      </div>
+    </React.Fragment>
+  );
+}
+
+function DiningCenter() {
+  const classes = useStyles();
+  return (
+    <React.Fragment>
+      <Title>Dining Centre</Title>
+      <Typography component="p" variant="h7">
+        Current Status:
+      </Typography>
+      <Typography component="p" variant="h5">
+        OPEN
+      </Typography>
+      <Typography component="p" variant="h7">
+        Current Number of People:
+      </Typography>
+      <Typography component="p" variant="h6">
+        40
+      </Typography>
+      <Typography component="p" variant="h6" color="#F44336">
+        Flow Rate: Middle {/*Should be red here! */}
+      </Typography>
+      
+      <Typography color="textSecondary" className={classes.depositContext}>
+      </Typography>
+      <div>
+        <Link color="primary" href="#" onClick={preventDefault}>
+          Check details
+        </Link>
+      </div>
+    </React.Fragment>
+  );
+}
+
+function Parkade() {
+  const classes = useStyles();
+  return (
+    <React.Fragment>
+      <Title>Art Parkade</Title>
+      <Typography component="p" variant="h7">
+        Current Status:
+      </Typography>
+      <Typography component="p" variant="h5">
+        OPEN
+      </Typography>
+      <Typography component="p" variant="h7">
+        Available Carslot :
+      </Typography>
+      <Typography component="p" variant="h4">
+        112
+      </Typography>
+      <Typography color="textSecondary" className={classes.depositContext}>
+      </Typography>
+      <div>
+        <Link color="primary" href="#" onClick={preventDefault}>
+          Check details
+        </Link>
+      </div>
+    </React.Fragment>
+  );
+}
+
+function Lot10() {
+  const classes = useStyles();
+  return (
+    <React.Fragment>
+      <Title>Lot 10</Title>
+      <Typography component="p" variant="h7">
+        Current Status:
+      </Typography>
+      <Typography component="p" variant="h5">
+        OPEN
+      </Typography>
+      <Typography component="p" variant="h7">
+        Available Carslots :
+      </Typography>
+      <Typography component="p" variant="h4">
+        67
+      </Typography>
+      <Typography color="textSecondary" className={classes.depositContext}>
+      </Typography>
+      <div>
+        <Link color="primary" href="#" onClick={preventDefault}>
+          Check details
+        </Link>
+      </div>
+    </React.Fragment>
+  );
+}
+
+function SwimmingPool() {
+  const classes = useStyles();
+  return (
+    <React.Fragment>
+      <Title>Swimming Pool</Title>
+      <Typography component="p" variant="h7">
+        Current Status:
+      </Typography>
+      <Typography component="p" variant="h5">
+        OPEN
+      </Typography>
+      <Typography component="p" variant="h7">
+        Current Number of People:
+      </Typography>
+      <Typography component="p" variant="h4">
+        37
+      </Typography>
+      <Typography color="textSecondary" className={classes.depositContext}>
+      </Typography>
+      <div>
+        <Link color="primary" href="#" onClick={preventDefault}>
+          Check details
+        </Link>
+      </div>
+    </React.Fragment>
+  );
+}
+
+function BasketballGym() {
+  const classes = useStyles();
+  return (
+    <React.Fragment>
+      <Title>Basketball Gym</Title>
+      <Typography component="p" variant="h7">
+        Current Status:
+      </Typography>
+      <Typography component="p" variant="h5">
+        OPEN
+      </Typography>
+      <Typography component="p" variant="h7">
+        Available Basketball Court:
+      </Typography>
+      <Typography component="p" variant="h4">
+        2
+      </Typography>
+      <Typography color="textSecondary" className={classes.depositContext}>
+      </Typography>
+      <div>
+        <Link color="primary" href="#" onClick={preventDefault}>
+          Check details
+        </Link>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default BasketballGym;

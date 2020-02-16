0fb363022ff3fc90ca958c244294a2608e793b22
Haohu Shen <haohu.shen@ucalgary.ca>
2020-02-15 20:52:28 -0700

    add StudyRepo, StudyEventRepo, StudyEventService, StudyService

--- backEndPart/src/main/java/com/calgaryhacks/demo/services/StudyEventService.java
+++ backEndPart/src/main/java/com/calgaryhacks/demo/services/StudyEventService.java
@@ -1,4 +1,39 @@
 package com.calgaryhacks.demo.services;
 
+import com.calgaryhacks.demo.domain.ExerciseEvent;
+import com.calgaryhacks.demo.domain.StudyEvent;
+import com.calgaryhacks.demo.repositories.StudyEventRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Date;
+import java.util.Random;
+
 public class StudyEventService {
+
+    @Autowired
+    private StudyEventRepo studyEventRepo;
+
+    public void createFakeData() {
+        Random rand    = new Random();
+        int            howMany = rand.nextInt(11);
+
+        for(int i=0; i<howMany; i++){
+            ExerciseEvent event = new ExerciseEvent();
+            event.setBuildingId(rand.nextInt(6));
+
+            event.setStartTime(new Date());
+            event.setEndTime(new Date());
+
+            event.setEventName("Event "+ i);
+            event.setDescription("Event description");
+        }
+    }
+
+    public StudyEvent findById(long id) {
+        return studyEventRepo.findById(id);
+    }
+
+    public Iterable<StudyEvent> returnAll(long id) {
+        return studyEventRepo.findAll();
+    }
 }
--- backEndPart/src/main/java/com/calgaryhacks/demo/services/StudyService.java
+++ backEndPart/src/main/java/com/calgaryhacks/demo/services/StudyService.java
@@ -12,16 +12,37 @@ public class StudyService {
     private StudyRepo studyRepo;
 
     public void createFakeData(){
+
         Study study1 = new Study();
-        study1.setName("TFDL");
-        study1.setCapacity(200);
+        study1.setName("Doucette Library");
+        study1.setCapacity(100);
 
         Study study2 = new Study();
-        study2.setName("Law Library");
-        study2.setCapacity(100);
+        study2.setName("Business Library");
+        study2.setCapacity(150);
+
+        Study study3 = new Study();
+        study3.setName("Health Sciences Library");
+        study3.setCapacity(150);
+
+        Study study4 = new Study();
+        study4.setName("Taylor Family Digital Library");
+        study4.setCapacity(300);
+
+        Study study5 = new Study();
+        study5.setName("Bennett Jones Law Library");
+        study5.setCapacity(100);
+
+        Study study6 = new Study();
+        study6.setName("Gallagher Library");
+        study6.setCapacity(150);
 
         studyRepo.save(study1);
         studyRepo.save(study2);
+        studyRepo.save(study3);
+        studyRepo.save(study4);
+        studyRepo.save(study5);
+        studyRepo.save(study6);
     }
 
     public Study findById(long id){
